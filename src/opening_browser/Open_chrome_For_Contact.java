e3bb2cac4a577291092e2ea942fbcbc11436afac
Akshata Chougula <Akshata Chougula@SMTC-R-0156>
2023-03-31 13:31:31 +0530

    automation test

---
+++ src/opening_browser/Open_chrome_For_Contact.java
@@ -0,0 +1,34 @@
+package opening_browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Open_chrome_For_Contact {
+public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://127.0.0.1:5000/contact");
+		Thread.sleep(5000);
+		String title = driver.getTitle();
+		System.out.println(title);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		String pageSource = driver.getPageSource();
+		System.out.println(pageSource);
+		String expectedtitle="ECODERS-contact-page";
+		if(expectedtitle.equals(title)) {
+			System.out.println("Title matched test case passed");
+		}
+		else {
+			System.out.println("Title not matched test case failed");
+		}
+		String excpectedurl="http://127.0.0.1:5000/contact";
+		if(excpectedurl.endsWith(currentUrl)) {
+			System.out.println("url matched test case passed");
+		}
+		else {
+			System.out.println("url not matched test case failed");
+		}
+		driver.quit();
+	}
+
+}
---
+++ src/opening_browser/Open_chrome_For_Testcase.java
@@ -0,0 +1,34 @@
+package opening_browser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Open_chrome_For_Testcase {
+public static void main(String[] args) throws InterruptedException {
+		WebDriver driver=new ChromeDriver();
+		driver.get("http://127.0.0.1:5000/testcase");
+		Thread.sleep(5000);
+		String title = driver.getTitle();
+		System.out.println(title);
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+		String pageSource = driver.getPageSource();
+		System.out.println(pageSource);
+		String expectedtitle="ECODERS-Testcase page";
+		if(expectedtitle.equals(title)) {
+			System.out.println("Title matched test case passed");
+		}
+		else {
+			System.out.println("Title not matched test case failed");
+		}
+		String excpectedurl="http://127.0.0.1:5000/testcase";
+		if(excpectedurl.endsWith(currentUrl)) {
+			System.out.println("url matched test case passed");
+		}
+		else {
+			System.out.println("url not matched test case failed");
+		}
+		driver.quit();
+	}
+
+}
